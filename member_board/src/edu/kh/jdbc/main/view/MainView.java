9590cc0e28cdde7e56a7127ea080f25d486a0971
YJuY <110653665+YJuY@users.noreply.github.com>
2022-09-22 16:33:14 +0900

    회원제게시판-end

--- member_board/src/edu/kh/jdbc/main/view/MainView.java
+++ member_board/src/edu/kh/jdbc/main/view/MainView.java
@@ -3,6 +3,7 @@ package edu.kh.jdbc.main.view;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import edu.kh.jdbc.board.view.BoardView;
 import edu.kh.jdbc.main.model.service.MainService;
 import edu.kh.jdbc.member.view.MemberView;
 import edu.kh.jdbc.member.vo.Member;
@@ -24,7 +25,9 @@ public class MainView {
 	// 회원 기능 메뉴 객체 생성
 	private MemberView memberView = new MemberView();
 	
-
+	// 게시판 기능 메뉴 객체 생성
+	private BoardView boardView = new BoardView();
+	
 	
 	
 	/**
@@ -81,11 +84,12 @@ public class MainView {
 					switch(input){
 					case 1: memberView.memberMenu(loginMember); break;
 					
+
+					case 2: boardView.boardMenu(); break;
+						// 회원 정보가 필요한 경우 static에서 얻어와 사용할 예정
 					
 					
 					
-					
-					case 2:  break;
 					case 0: // 로그아웃 == loginMember가 참조하는 객체 없음( == null)
 						// 로그인 == loginMember가 참조하는 객체 존재
 						loginMember = null;
@@ -93,6 +97,7 @@ public class MainView {
 						
 						input = -1; // do-while문이 종료되지 않도록 0이 아닌 값으로 변경
 						break;
+						
 					case 99:  
 						System.out.println("프로그램 종료");
 //						input = 0; //-> do-while 조건식을 false로 만듦
@@ -267,13 +272,16 @@ public class MainView {
 	    *     댓글 목록(작성일 오름차순 )
 	    *     
 	    *     
-	    *     2-1. 게시글 수정 (자신의 게시글만)
-	    *     2-2. 게시글 삭제 (자신의 게시글만)
 	    *     
-	    *     2-3. 댓글 작성
-	    *     2-4. 댓글 수정 (자신의 댓글만)
-	    *     2-5. 댓글 삭제 (자신의 댓글만)
-	    * 
+	    *     
+	    *     2-1. 댓글 작성
+	    *     2-2. 댓글 수정 (자신의 댓글만)
+	    *     2-3. 댓글 삭제 (자신의 댓글만)
+	    *     
+	    *     // 자신이 작성한 글 일때만 메뉴 노출
+	    * 	  2-4. 게시글 수정
+	    *     2-5. 게시글 삭제
+	    *     
 	    * 3. 게시글 작성(제목, 내용 INSERT) 
 	    *    -> 작성 성공 시 상세 조회 수행
 	    * 
